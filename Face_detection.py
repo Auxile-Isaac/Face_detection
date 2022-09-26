ffa05bc8662e9f90579edee6b6ea2b73c78a5701
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-26 17:27:50 +0200

    detect the coordinates of the face

--- Face_detection.py
+++ Face_detection.py
@@ -1,16 +1,20 @@
 import cv2
 
 # load some pre-trained data on face frontals from opencv (haar cascade algorithm)
-trained_face_dat = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#choose an image to detect faces in
-img = cv2.imread('img1.jpeg')
+img = cv2.imread('img2.jpeg')	#choose an image to detect faces in
 
-# show the image 
-cv2.imshow('Face detected', img)
+# convert img to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# waits untill any key is pressed
-cv2.waitKey()
+# Detect faces, and return the coordinates of the rectangle
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+print(face_coordinates)
+
+# cv2.imshow('Face detected', grayscaled_img)	# show the image 
+cv2.waitKey()	# waits untill any key is pressed
 
 
-print("hell yeah... it's working!")
+print("hell yeah... it's working!") # just to make sure my code works perfectly
