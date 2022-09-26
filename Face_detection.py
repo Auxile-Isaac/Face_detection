3a6afd7abb1da45db8bedc6c656715eb5db5357e
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-26 18:05:29 +0200

    detects one face at time

--- Face_detection.py
+++ Face_detection.py
@@ -3,17 +3,20 @@ import cv2
 # load some pre-trained data on face frontals from opencv (haar cascade algorithm)
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('img2.jpeg')	#choose an image to detect faces in
+img = cv2.imread('img3.jpeg')	#choose an image to detect faces in
 
 # convert img to grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces, and return the coordinates of the rectangle
+# Detect faces, and return the coordinates of the face
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-print(face_coordinates)
+# Draw rectangles around the faces
+(x, y, w, h) = face_coordinates[0]
+cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# print(face_coordinates)
 
-# cv2.imshow('Face detected', grayscaled_img)	# show the image 
+cv2.imshow('Face detected', img)	# show the image 
 cv2.waitKey()	# waits untill any key is pressed
 
 
