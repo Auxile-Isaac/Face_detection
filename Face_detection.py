4f96a23fa6ee45cd0417b2dc60cfee9208c210f8
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-26 16:58:44 +0200

    import and show image

--- Face_detection.py
+++ Face_detection.py
@@ -1,7 +1,16 @@
 import cv2
 
+# load some pre-trained data on face frontals from opencv (haar cascade algorithm)
+trained_face_dat = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#choose an image to detect faces in
+img = cv2.imread('img1.jpeg')
+
+# show the image 
+cv2.imshow('Face detected', img)
+
+# waits untill any key is pressed
+cv2.waitKey()
 
 
-
-
-print("fuck yeah... it's working!")
+print("hell yeah... it's working!")
