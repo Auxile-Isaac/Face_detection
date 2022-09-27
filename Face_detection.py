07d4803650dd13d7e7335d56e22d94d9c95bd9bb
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-27 17:13:52 +0200

    video face detection

--- Face_detection.py
+++ Face_detection.py
@@ -1,13 +1,33 @@
 import cv2
+from random import randrange
 
 # load some pre-trained data on face frontals from opencv (haar cascade algorithm)
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('img5.jpeg')	#choose an image to detect faces in
+#img = cv2.imread('img5.jpeg')	#==> hardcoded choosen image to detect faces in
+# capture video from webcam
+webcam = cv2.VideoCapture(0)
 
-# convert img to grayscale
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# iterate forever over frames
 
+while True:
+
+	# read the current frame
+	successfulFrame_read, frame = webcam.read()
+	
+	# convert frame to grayscale
+	grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# Detect faces, and return the coordinates of the face
+	face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+	# loop over the image and detect all faces
+	for (x, y, w, h) in face_coordinates:
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)	# ==> randrange() assigns a random value from 0 to 255
+
+	cv2.imshow('Face detected', frame)	# Display the image with the faces
+	cv2.waitKey(1)	# waits untill any key is pressed
+"""
 # Detect faces, and return the coordinates of the face
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
@@ -16,11 +36,14 @@ face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
 # loop over the image and detect all faces
 for (x, y, w, h) in face_coordinates:
-	cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)	# ==> randrange() assigns a random value from 0 to 255
 # print(face_coordinates)
 
-cv2.imshow('Face detected', img)	# show the image 
+cv2.imshow('Face detected', img)	# Display the image with the faces
 cv2.waitKey()	# waits untill any key is pressed
+
+"""
 
 
 print("hell yeah... it's working!") # just to make sure my code works perfectly
+
